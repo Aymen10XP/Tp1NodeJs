c512574ad4bab3dcedf733eec86b5961cea6464e
Aymen10XP <125681512+Aymen10XP@users.noreply.github.com>
2026-02-13 20:34:55 +0100

    exComplete

---
+++ exercice/main.js
@@ -0,0 +1,8 @@
+var {searchElement} = require('./searchElement')
+var {data} = require('./data')
+searchElement(data, function (err, result) {
+    if (err)
+        console.error("erreur :" + err)
+    else
+        console.log(data.filtre + " existe a la position " + result)
+});
---
+++ exercice/searchElement.js
@@ -0,0 +1,6 @@
+exports.searchElement = function (data, callback) {
+    for (var i = 0; i < data.tableau.length; i++)
+        if (data.tableau[i] == data.filtre)
+            return callback(null, i);
+    return callback('Element ' + data.filtre + ' non retrouvé dans le tableau');
+};
